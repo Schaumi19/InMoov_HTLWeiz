5118c27667d5d7defcedb129355fff88d84687f0
Schaumi19 <51927635+Schaumi19@users.noreply.github.com>
2021-02-14 13:30:41 +0100

    VESC Current change added

--- getVescValues/getVescValues.ino
+++ getVescValues/getVescValues.ino
@@ -1,33 +1,28 @@
 
 
 /*
-  Name:    getVescValues.ino
-  Created: 19-08-2018
-  Author:  SolidGeek
-  Description:  This example is made using a Arduino Micro (Atmega32u4) that has a HardwareSerial port (Serial1) seperated from the Serial port. 
-                A Arduino Nano or Uno that only has one Serial port will not be able to display the data returned.
+ 
+  Author: Manuel Schaumberger
+  
+  Library Author:  SolidGeek
+  
 */
 
 #include <VescUart.h>
-//#include <Adafruit_NeoPixel.h>
-//#include <avr/power.h>
 
-#define Sel            2
-#define VJoystick      1
-#define HJoystick      0
 
-//Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, RGB_PIN, NEO_GRB + NEO_KHZ800);
+const byte Sel = 2;
+const byte VJoystick = 1;
+const byte HJoystick = 0;
 
 /** Initiate VescUart class */
 VescUart UART;
 
-int delayval = 0;
-
 int RPM1 = 0;
 int RPM2 = 0;
 
-int sRPM1 = 000;
-int sRPM2 = 000;
+int sRPM1 = 0;
+int sRPM2 = 0;
 
 int rRPM1 = 0;
 int rRPM2 = 0;
@@ -35,44 +30,35 @@ int rRPM2 = 0;
 void setup() {
 
   pinMode(VJoystick, INPUT);
+  pinMode(HJoystick, INPUT);
   pinMode(Sel, INPUT_PULLUP);
-  /** Setup Serial port to display data */
+  
+  // Setup Serial port to display data
   Serial.begin(9600);
 
-  /** Setup UART port (Serial1 on Atmega32u4) */
+  // Setup UART port (für VESC) 
   Serial1.begin(115200);
   Serial2.begin(115200);
   
   while (!Serial) {;}
 
-/*  pixels.begin();
 
-
-  for(int i=0;i<NUMPIXELS;i++){
-    pixels.setPixelColor(i, pixels.Color(55,55,55)); // Moderately bright green color.
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(delayval); // Delay for a period of time (in milliseconds).
-  }
-  for(int i=0;i<NUMPIXELS;i++){
-    pixels.setPixelColor(i, pixels.Color(0,0,1)); // Moderately bright green color.
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(delayval); // Delay for a period of time (in milliseconds).
-  }
-  /** Define which ports to use as UART */
-  UART.nunchuck.valueY = 127;
 }
 
 void loop() {
-RPM1 = 0;
-RPM2 = 0;
-if(digitalRead(Sel) == LOW){
-  //UART.setNunchuckValues();
-  sRPM1 = 18 * (analogRead(VJoystick)-514) ;
-  sRPM2 = 18 * (analogRead(VJoystick)-514);
-  Serial.println(sRPM1);
 
-  RPM1 = sRPM1;
-  RPM2 = sRPM2;
+if (Serial.available()) {
+    byte Current = Serial.read();
+    SetVESCCurrent(Current);
+  }
+
+  
+
+if(digitalRead(Sel) == LOW){
+
+  RPM1 = 18 * (analogRead(VJoystick)-514) ;
+  RPM2 = 18 * (analogRead(VJoystick)-514);
+  Serial.println(sRPM1);
 
   if(abs(RPM1) < 100){
     RPM1 = 0;
@@ -82,40 +68,7 @@ if(digitalRead(Sel) == LOW){
   }
  }
 
-/*
-if(sRPM1 > 0){
-    if(((float)sRPM1+200)/sRPM2 < (float)rRPM1/rRPM2){
-  RPM1 = rRPM2;
-}
-else{
-  RPM1 = sRPM1;
-}
-} else{
-    if(((float)sRPM1-200)/sRPM2 < (float)rRPM1/rRPM2){
-  RPM1 = rRPM2;
-}
-else{
-  RPM1 = sRPM1;
-}
-}
-if(sRPM2 > 0){
-  
-if((float)sRPM1-200/sRPM2 > ((float)rRPM1)/rRPM2){
-  RPM2 = rRPM1;
-}
-else{
-  RPM2 = sRPM2;
-}
-}else{
-  if((float)sRPM1+200/sRPM2 > ((float)rRPM1)/rRPM2){
-  RPM2 = rRPM1;
-}
-else{
-  RPM2 = sRPM2;
-}
-}
 
-*/
   /** Call the function getVescValues() to acquire data from VESC */
   UART.setSerialPort(&Serial1);
   
@@ -138,9 +91,7 @@ else{
     Serial.println("Failed to get data!");
     
   }
-  //UART.nunchuck.lowerButton = true;
-  
-  //UART.setNunchuckValues();
+
   
   UART.setRPM(RPM1); //0 - 9000
 
@@ -165,8 +116,7 @@ else{
     Serial.println("Failed to get data!");
     
   }
-  //UART.nunchuck.lowerButton = true;
-  //UART.setNunchuckValues();
+
   UART.setRPM(RPM2); //0 - 9000
 
 
@@ -174,6 +124,11 @@ else{
 
   //Serial.println();
 
+}
 
-  
+void SetVESCCurrent(byte current){
+  UART.setSerialPort(&Serial1);
+  UART.setCurrent(current);
+  UART.setSerialPort(&Serial2);
+  UART.setCurrent(current);
 }
