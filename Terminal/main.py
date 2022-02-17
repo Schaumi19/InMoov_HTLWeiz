d780bbde12b698b9e1fc6b4e7d3a548f235a25da
Thomasderguetige <thomasbaumkircher10@gmail.com>
2022-02-17 14:24:46 +0100

    started working on Terminal

---
+++ Terminal/main.py
@@ -0,0 +1,57 @@
+# -- Global imports -- #
+
+import menu
+import sys
+import glob
+import serial
+
+
+
+# -- Global variable declaration -- #
+
+platform = sys.platform
+baudrate = 112500
+
+
+
+# -- Global script -- #
+
+def main():
+    serial_arr = setup_ports()
+    for s in serial_arr:
+        print(str(s))
+
+    menu.main_menu()
+
+
+
+def setup_ports():
+    """Returns all serial ports that can be connected to
+    """
+
+    temp_ports = list()
+
+    if platform.startswith("win"):
+        temp_ports = serial.tools.list_ports()
+
+    elif platform.startswith("linux"):
+        temp_ports = glob.glob("/dev/tty[A-Za-z]*")
+
+    return_arr = list()
+    for port in temp_ports:
+        print(port)
+        try:
+            s = serial.Serial(port=port, baudrate=baudrate)
+            s.close()
+            return_arr.append(s)
+        except serial.SerialException:
+            pass
+
+    return return_arr
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ Terminal/menu.py
@@ -0,0 +1,4 @@
+def main_menu():
+    """This is the main menu of the InMoov terminal
+    """
+    
