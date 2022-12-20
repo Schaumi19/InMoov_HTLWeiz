3fc7a60fce9456e5d1c736ca80bd19676967f21a
Schaumi19 <manuelschaumberger@gmx.at>
2022-12-20 08:42:21 +0100

    changed to makro parameters

--- AktuatorCode/AktuatorParameter.h
+++ AktuatorCode/AktuatorParameter.h
@@ -6,6 +6,42 @@
 #define ACP_B1 3       // change for different boards
                         //3 = Left hand side, 4 = Middle, 5 = Right hand side       different Code:(1 = BoardMotorController, 2 = RGB)
 
+#define A3_used {true,true,true,true}
+#define A3_min_angle {0, 0, 0, 0}
+#define A3_max_angle {180, 180, 180, 180}
+#define A3_min_pot {574, 258, 181, 450}
+#define A3_max_pot {846, 830, 855, 963}
+#define A3_reversed_output {true, true, true, false}
+#define A3_reversed_input {false, false, false, false}
+#define A3_goalDeadzone {6,6,6,6}
+#define A3_maxSpeed {255,255,255,255}
+#define A3_maxAngularSpeed {0,0,0,0}
+#define A3_ContinuousMovement {0,0,0,0}
+
+#define A4_used {true,true,false,false}
+#define A4_min_angle {0, 0, 0, 0}
+#define A4_max_angle {180, 180, 180, 180}
+#define A4_min_pot {384, 271, 0, 0}
+#define A4_max_pot {661, 776, 1000, 1000}
+#define A4_reversed_output {false, false, false, false}
+#define A4_reversed_input {false, false, false, false}
+#define A4_goalDeadzone {6,6,6,6}
+#define A4_maxSpeed {200,255,255,255}
+#define A4_maxAngularSpeed {200,200,0,0}
+#define A4_ContinuousMovement {0,0,0,0}
+
+#define A5_used {true,true,true,true}
+#define A5_min_angle {0, 0, 0, 0}
+#define A5_max_angle {180, 180, 180, 180}
+#define A5_min_pot {222, 281, 91, 340}
+#define A5_max_pot {492, 810, 670, 844}
+#define A5_reversed_output {false, true, true, true}
+#define A5_reversed_input {false, false, false, false}
+#define A5_goalDeadzone {6,6,6,6}
+#define A5_maxSpeed {255,255,255,255}
+#define A5_maxAngularSpeed {0,0,0,0}
+#define A5_ContinuousMovement {0,0,0,0}
+
 struct AktuatorParameter
 {
     bool used[4];
@@ -21,45 +57,11 @@ struct AktuatorParameter
     byte ContinuousMovement[4];
 };
 
-AktuatorParameter aktuatorParameters[3]={{{true,true,true,true},{0, 0, 0, 0},{180, 180, 180, 180},{574, 258, 181, 450},{846, 830, 855, 963},{true, true, true, false},{false, false, false, false},{6,6,6,6},{255,255,255,255},{0,0,0,0},{0,0,0,0}},{{true,true,true,true},{0, 0, 0, 0},{180, 180, 180, 180},{574, 258, 181, 450},{846, 830, 855, 963},{true, true, true, false},{false, false, false, false},{6,6,6,6},{255,255,255,255},{0,0,0,0},{0,0,0,0}},{{true,true,true,true},{0, 0, 0, 0},{180, 180, 180, 180},{574, 258, 181, 450},{846, 830, 855, 963},{true, true, true, false},{false, false, false, false},{6,6,6,6},{255,255,255,255},{0,0,0,0},{0,0,0,0}}};
-/*
-aktuatorParameters[0].used = {true,true,true,true};
-aktuatorParameters[0].min_angle = {0, 0, 0, 0};
-aktuatorParameters[0].max_angle = {180, 180, 180, 180};
-aktuatorParameters[0].min_pot = {574, 258, 181, 450};
-aktuatorParameters[0].max_pot = {846, 830, 855, 963};
-aktuatorParameters[0].reversed_output = {true, true, true, false};
-aktuatorParameters[0].reversed_input = {false, false, false, false};
-aktuatorParameters[0].goalDeadzone = {6,6,6,6};
-aktuatorParameters[0].maxSpeed = {255,255,255,255};
-aktuatorParameters[0].maxAngularSpeed = {0,0,0,0};
-aktuatorParameters[0].ContinuousMovement = {0,0,0,0};
-
-aktuatorParameters[1].used[4] = {true,true,false,false};
-aktuatorParameters[1].min_angle = {0, 0, 0, 0}; //45-115 screw drive
-aktuatorParameters[1].max_angle = {180, 180, 180, 180};
-aktuatorParameters[1].min_pot = {384, 271, 0, 0};
-aktuatorParameters[1].max_pot = {661, 776, 1000, 1000};
-aktuatorParameters[1].reversed_output = {false, false, false, false};
-aktuatorParameters[1].reversed_input = {false, false, false, false};
-aktuatorParameters[1].goalDeadzone = {6,6,6,6};
-aktuatorParameters[1]. maxSpeed = {200,255,255,255};
-aktuatorParameters[1].maxAngularSpeed = {200,200,0,0};
-aktuatorParameters[1].ContinuousMovement = {0,0,0,0};
-
-aktuatorParameters[2].used = {true,true,true,true};
-aktuatorParameters[2].min_angle = {0, 0, 0, 0};
-aktuatorParameters[2].max_angle = {180, 180, 180, 180};
-aktuatorParameters[2].min_pot = {222, 281, 91, 340};
-aktuatorParameters[2].max_pot = {492, 810, 670, 844};
-aktuatorParameters[2].reversed_output = {false, true, true, true};
-aktuatorParameters[2].reversed_input = {false, false, false, false};
-aktuatorParameters[2].goalDeadzone = {6,6,6,6};
-aktuatorParameters[2].maxSpeed = {255,255,255,255};
-aktuatorParameters[2].maxAngularSpeed = {0,0,0,0};
-aktuatorParameters[2].ContinuousMovement = {0,0,0,0};
-    
-*/
+AktuatorParameter aktuatorParameters[3]={
+    {A3_used,A3_min_angle,A3_max_angle,A3_min_pot,A3_max_pot,A3_reversed_output,A3_reversed_input,A3_goalDeadzone,A3_maxSpeed,A3_maxAngularSpeed,A3_ContinuousMovement},
+    {A4_used,A4_min_angle,A4_max_angle,A4_min_pot,A4_max_pot,A4_reversed_output,A4_reversed_input,A4_goalDeadzone,A4_maxSpeed,A4_maxAngularSpeed,A4_ContinuousMovement},
+    {A5_used,A5_min_angle,A5_max_angle,A5_min_pot,A5_max_pot,A5_reversed_output,A5_reversed_input,A5_goalDeadzone,A5_maxSpeed,A5_maxAngularSpeed,A5_ContinuousMovement}
+    };
 
 //general stuff:
 const long Baudrate = 115200;
