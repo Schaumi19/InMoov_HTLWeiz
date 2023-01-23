e8cef2f3f71771c9dd21eae68fb6cba17ba25d7a
Schaumi19 <51927635+Schaumi19@users.noreply.github.com>
2023-01-23 17:12:24 +0100

    Update test.py

--- Recognition/Sample program/test.py
+++ Recognition/Sample program/test.py
@@ -1,1 +1,15 @@
-
+import cv2
+from cv2 import imshow
+#$ for d in /dev/video* ; do echo $d ; v4l2-ctl --device=$d -D --list-formats  ; echo '===============' ; done
+cap = cv2.VideoCapture("/dev/video4")
+while cap.isOpened():
+	success, frame = cap.read()
+	if not success:
+		print("Error")
+		continue
+	
+	cv2.imshow("Image", frame)
+	key = cv2.waitKey(1)
+	
+	if key == 27:
+		break
