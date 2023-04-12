467fd274d767f5aa67bd2f3eaf74130c3625a4a3
Andrea Windisch <windis180093.htlweiz.at>
2023-04-12 15:06:48 +0200

    Added RockPaperSissor Cliet for simple Terminal use

---
+++ Terminal/RockPaperScissor_Client.py
@@ -0,0 +1,27 @@
+from subscripts import ports
+from menus.gestures import Gestures
+from time import sleep
+from menus.games.RockPaperScissor.RockPaperScissors import game as RockPaperScissors
+from menus.games.RockPaperScissor import speech
+
+
+baudrate = 115200
+serial_port = None
+
+def main():
+    global serial_port
+    serial_arr = ports.setup_ports(baudrate)
+    serial_port = serial_arr[0]
+    serial_port.open()
+    print(serial_port)
+    gestures = Gestures(serial_port)
+    
+    ###YOUR CODE HERE###
+    gestures.normal()
+    RockPaperScissors(serial_port)
+    
+
+    
+if __name__ == "__main__":
+    main()
+    
