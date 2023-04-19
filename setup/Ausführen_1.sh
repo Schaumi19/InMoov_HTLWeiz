3ef63812f75b35d6e0fa091329fd912430f630ee
Andrea Windisch <windis180093.htlweiz.at>
2023-04-19 17:32:32 +0200

    setup finished

--- setup/Ausführen_1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-xterm -hold -e 'python3 InMoov_HTLWeiz/Terminal/menus/games/RockPaperScissor/RPS_server.py'
--- setup/Ausführen_2.sh
+++
@@ -1,7 +0,0 @@
-[Desktop Entry]
-Name=Mein Skript
-Exec=sudo python3 InMoov_HTLWeiz/Terminal/RockPaperScissor_Client.py
-Icon=/InMoov_HTLWeiz/setup/icon.png
-Terminal=false
-Type=Application
-Categories=Utility
