f3d903b352d33a83860637639353dd081dd5d0c9
Andrea Windisch <windis180093.htlweiz.at>
2023-04-19 15:09:06 +0200

    small changes in setup

--- setup/Ausführen_1.sh
+++ setup/Ausführen_1.sh
@@ -1,2 +1,2 @@
-#!bin/bash
+#!/bin/bash
 python3 /InMoov_HTLWeiz/Terminal/menus/games/RockPaperScissor/RPS_server.py
--- setup/Ausführen_2.sh
+++ setup/Ausführen_2.sh
@@ -1,2 +1,2 @@
-#!bin/bash
+#!/bin/bash
 sudo python3 /InMoov_HTLWeiz/Terminal/RockPaperScissor_Client.py
